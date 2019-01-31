9cd1e0b758bc95642d7bac901af45dc1929fd645
Samuel Gutema <samuel.gutema@gmail.com>
2019-01-31 18:57:17 -0500

    final version

--- main/javascript /script.js
+++ main/javascript /script.js
@@ -37,6 +37,7 @@
         frequency: tFrequency
     }; 
 
+    
 
     // upload employee data to the database 
 
@@ -78,14 +79,94 @@
     var tDestination = childSnapshot.val().destination;
     var tFirstTrain = childSnapshot.val().first;
     var tFrequency = childSnapshot.val().frequency;
+    // var tMinutesAway = childSnapshot.val().minutesAway
 
 
 
-    // var trainFrequency = 30;
+    
 
-    // var trainArrival= tFirstTrain - trainFrequency
+    //adding clock 
+  
 
-    // var trainAway = trainArrival + trainFrequency
+    // function updateTime () {
+    //     var now = moment();
+    //     var timeReadable = now.format("s"); 
+
+    //     tFrequency.textContent = timeReadable;
+    //     setInterval(updateTime, 1000);
+
+
+    // }
+    // updateTime()
+    
+
+    // var frequencyTimer = tFrequency
+    
+    //  tFrequency=  moment.HTML5_FMT.TIME
+    //  setInterval(frequencyTimer, 1000)
+
+
+    // var frequencyTimer = moment.unix(tFrequency).format("hh:mm")
+    
+
+// // tFrequency = moment.HTML5_FMT.TIME
+  
+// // setInterval(tFrequency, 1000)
+
+    // var currentTime = moment(tFirstTrain).format("hh:mm")
+
+    var firstTimeConverted = moment(tFirstTrain, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % tFrequency;
+
+
+
+    
+
+    
+
+    
+       var minutesAway = tFrequency - tRemainder
+
+       var nextArrival = moment().add(minutesAway, "minutes");
+       nextArrival = moment(nextArrival).format("hh:mm A");
+
+
+//new 
+    //   var tFrequency = 3;
+
+      // Time is 3:30 AM
+    //   var firstTime = "03:30";
+  
+    //   // First Time (pushed back 1 year to make sure it comes before current time)
+    //   var firstTimeConverted = moment(tFirstTrain, "HH:mm").subtract(1, "years");
+     
+  
+    //   // Current Time
+    //   var currentTime = moment();
+
+    //   currentTime = moment(tFirstTrain).format("hh:mm");
+  
+    //   // Difference between the times
+    //   var diffTime = moment().diff(moment(currentTime), "minutes");
+  
+    //   // Time apart (remainder)
+    //   var tRemainder = diffTime % tFrequency;
+  
+    //   // Minute Until Train
+    //   var tMinutesTillTrain = tFrequency - tRemainder;
+  
+    //   // Next Train
+    //   var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    //                 nextTrain=   moment(nextTrain).format("hh:mm A");
+
+
+
+
+
+
+
+
 
     var newRow = $("<tr>").append(
 
@@ -93,8 +174,9 @@
     $("<td>").text(tDestination),
     $("<td>").text(tFrequency),
     // $("<td>").text(trainArrival),
-    // $("<td>").text(empRate),
-    $("<td>").text(tFirstTrain)
+    $("<td>").text(nextArrival),
+    $("<td>").text(minutesAway)
+
 
     ); 
 
@@ -107,3 +189,42 @@
 
 
   });
+
+
+
+
+
+
+
+
+
+//   $("#add-table-btn").on("click",  function updateTime () {
+//         var now = moment();
+//         var timeReadable = now.format(moment.HTML5_FMT.TIME); 
+
+//         tFrequency.textContent = timeReadable;
+//         setInterval(updateTime, 1000);
+
+
+//     })
+//     updateTime()
+
+
+
+
+
+
+
+
+
+  function clockStart() {
+    setInterval(function() {
+               date = new Date()
+               let hour = date.getHours();
+               let minutes = date.getMinutes();
+               let seconds = date.getSeconds();
+               document.getElementById("demo").innerHTML = hour + ":"+ minutes + ":" + seconds;
+  }, 1000);
+  }
+
+
